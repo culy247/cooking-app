c909741acc4dd678ddd1307010cb9775dac672fc
Phuc Tran <huyphuc0906@gmail.com>
2020-08-11 05:44:56 +0700

    Clean up old code to prepare for new architecture

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,6 @@
 import 'package:cooking/generated/l10n.dart';
 import 'package:cooking/router/routes.dart';
 import 'package:cooking/screen/home/home_screen.dart';
-import 'package:cooking/store/test_store.dart';
 import 'package:cooking/theme/colors.dart';
 import 'package:cooking/theme/fonts.dart';
 import 'package:fluro/fluro.dart';
@@ -30,54 +29,49 @@ class _AppState extends State<App> {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: AppColors.primary,
     ));
-    return MultiProvider(
-      providers: [
-        Provider<TestStore>.value(value: TestStore()),
-      ],
-      child: MaterialApp(
-          color: AppColors.primary,
-          title: '',
-          localizationsDelegates: [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: S.delegate.supportedLocales,
-          localeResolutionCallback:
-              (Locale locale, Iterable<Locale> supportedLocales) {
-            if (locale == null) {
-              debugPrint("*language locale is null!!!");
-              return supportedLocales.first;
-            }
+    return MaterialApp(
+        color: AppColors.primary,
+        title: '',
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        localeResolutionCallback:
+            (Locale locale, Iterable<Locale> supportedLocales) {
+          if (locale == null) {
+            debugPrint("*language locale is null!!!");
+            return supportedLocales.first;
+          }
 
-            for (Locale supportedLocale in supportedLocales) {
-              if (locale != null) {
-                if (supportedLocale.languageCode == locale.languageCode ||
-                    supportedLocale.countryCode == locale.countryCode) {
-                  return supportedLocale;
-                }
+          for (Locale supportedLocale in supportedLocales) {
+            if (locale != null) {
+              if (supportedLocale.languageCode == locale.languageCode ||
+                  supportedLocale.countryCode == locale.countryCode) {
+                return supportedLocale;
               }
             }
-            return supportedLocales.first;
-          },
-          onGenerateRoute: App.router.generator,
-          theme: ThemeData(
-              primaryColor: AppColors.primary,
-              //          accentColor: AppColors.primary,
-              fontFamily: Fonts.robotoRegular,
-              cupertinoOverrideTheme: CupertinoThemeData(
-                  textTheme: CupertinoTextThemeData(
-                      dateTimePickerTextStyle: TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.w300))),
-              primaryTextTheme: TextTheme(
-                headline6:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              primaryIconTheme: IconThemeData(color: Colors.white)),
-          //home: TutorialScreen(),
-          home: HomeScreen()),
-    );
+          }
+          return supportedLocales.first;
+        },
+        onGenerateRoute: App.router.generator,
+        theme: ThemeData(
+            primaryColor: AppColors.primary,
+            //          accentColor: AppColors.primary,
+            fontFamily: Fonts.robotoRegular,
+            cupertinoOverrideTheme: CupertinoThemeData(
+                textTheme: CupertinoTextThemeData(
+                    dateTimePickerTextStyle: TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.w300))),
+            primaryTextTheme: TextTheme(
+              headline6:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            primaryIconTheme: IconThemeData(color: Colors.white)),
+        //home: TutorialScreen(),
+        home: HomeScreen());
   }
 
 }
